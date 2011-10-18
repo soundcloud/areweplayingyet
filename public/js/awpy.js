dd889195f9b98cf8ec663debd4a9f287262fdf6b
Tomás Senart <tomas@soundcloud.com>
2011-10-18 14:28:53 +0200

    Finished AWPY0

--- public/js/awpy.js
+++ public/js/awpy.js
@@ -18,6 +18,13 @@ var AWPY = {
   }
 };
 
+AWPY.audio = (function() {
+  var audio;
+  return function() {
+    return audio = audio || new Audio();
+  };
+}());
+
 AWPY.tests = (function() {
   var list = [];
 
@@ -37,15 +44,19 @@ AWPY.tests = (function() {
   };
 
   var save = function() {
-    var data = {}, i, len;
+    var data = {}, i, len, url;
     for (i = 0, len = list.length; i < len; i++) {
       data[list[i].name] = list[i].result ? 1 : 0;
     }
 
     window._bTestResults = data;
 
-    $.getJSON('http://www.browserscope.org/user/beacon/' + AWPY.config.browserscope.key + '?callback=?', {
-      sandboxid: AWPY.config.browserscope.sandboxKey
+    url = 'http://www.browserscope.org/user/beacon/' +
+          AWPY.config.browserscope.key +
+          '?sandboxid=' + AWPY.config.browserscope.sandboxKey +
+          '&callback=?';
+    $.getJSON(url, function() {
+      console.log(this, arguments);
     });
   };
 
@@ -74,30 +85,102 @@ AWPY.sounds = {
 AWPY.tests.init([
   {
     name: 'AWPY0',
-    key: 'agt1YS1wcm9maWxlcnINCxIEVGVzdBixnOcJDA',
-    description: 'Play unbuffered position. (Seeking)',
+    description: 'Seeking to unbuffered position with seamless playback.',
     assert: function(finish) {
-      var audio = AWPY.audio = new Audio(),
+      var audio = AWPY.audio(),
           sound = AWPY.sounds.huge,
-          seekDuration = 0;
+          timeout, finished = false,
+          nseeks = 0, seekPoints = [0.7, 0.1, 0.9, 0.5, 0.3],
+          seekedTime;
 
-      audio.addEventListener('seeking', function() {
-        seekDuration = new Date();
-      });
+      var doSeek = function(seekPoint) {
+        audio.currentTime = (sound.duration * seekPoint) / 1000;
+        timeout = setTimeout(function() {
+          if (finished) return;
+          finished = true;
+          finish(false);
+        }, 500);
+      };
+
+      audio.volume = 0;
 
       audio.addEventListener('seeked', function() {
-        seekDuration = (new Date()) - seekDuration;
-        finish(seekDuration < 100);
+        if (finished) return;
+
+        clearTimeout(timeout);
+
+        if (++nseeks === seekPoints.length) {
+          seekedTime = audio.currentTime;
+          audio.play();
+          setTimeout(function() {
+            // Assert that audio canplaythrough after seek
+            finish(audio.currentTime > seekedTime);
+            audio.pause();
+          }, 5000);
+        } else {
+          doSeek(seekPoints[nseeks]);
+        }
       });
 
-      audio.addEventListener('canplay', function() {
-        var seekTime = (audio.buffered && audio.buffered.length) ?
-          audio.buffered.end(0) + 60 : (sound.duration / 3) / 1000;
-        audio.currentTime = seekTime;
+      audio.addEventListener('loadedmetadata', function() {
+        doSeek(seekPoints[nseeks]);
       });
 
+      audio.setAttribute('preload', 'metadata');
       audio.setAttribute('src', AWPY.sounds.huge.stream_url);
-      audio.load();
     }
   }
 ]);
+
+AWPY.player = function() {
+  var $container = $('#player'),
+  $prop = $('#prop'),
+  sound = AWPY.sounds.huge,
+  active = -1, anim,
+  props = {
+    timeupdate: function(audio) {
+      return audio.currentTime;
+    },
+    progress: function(audio) {
+      return (audio.buffered && audio.buffered.length) ? audio.buffered.end(0) : 0;
+    },
+    seekable: function(audio) {
+      return (audio.seekable && audio.seekable.length) ? audio.seekable.end(0) : 0;
+    }
+  },
+  intervals = {};
+
+  $container.prepend($('<img src="' + sound.waveform_url + '">')).css('background', '#ccc');
+
+  anim = function(prop) {
+    var progressX = (props[prop](AWPY.audio()) * $container.width()) / (AWPY.sounds.huge.duration / 1e3) | 0;
+    console.log(prop, progressX)
+    $container.find('#' + prop).width(progressX);
+  };
+
+  for (var k in props) {
+    intervals[k] && clearInterval(intervals[k]);
+    intervals[k] = setInterval(function() { anim(k); }, 50)
+  }
+};
+
+
+
+//
+// Possible benchmarking
+//
+//
+// preload attribute
+// paused, played, muted, mute()
+// buffered
+// autoplay
+// all events
+// metadata preload - no preload to play delay
+// redirects in streams (https->http, x-domain)
+// redirects in streams invalidate (s3)
+// the density of timeupdate events
+// switching streams (android)
+//
+//
+// supports more than ogg?
+//

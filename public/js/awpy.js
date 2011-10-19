c8c14043cc5e3b04b779fd98e8cf5f24b97e5ecd
Tomás Senart <tomas@soundcloud.com>
2011-10-19 12:33:02 +0200

    Removed name from tests, index is enough. Improved finishWrapper and added a timeout finish for every test (10s)

--- public/js/awpy.js
+++ public/js/awpy.js
@@ -21,67 +21,65 @@ var AWPY = {
 AWPY.tests = (function() {
   var list = [];
 
-  var init = function(tests) {
-    list = tests;
-  };
-
-  var run = function(callback) {
-    list.forEach(function(test) {
-      test.assert(function(result) {
-        test.result = result;
-        callback.call(test);
-      });
-    });
-  };
-
-  var save = function() {
-    var data = {}, i, len, url;
-    for (i = 0, len = list.length; i < len; i++) {
-      data[list[i].name] = list[i].result ? 1 : 0;
-    }
-
-    window._bTestResults = data;
-
-    url = 'http://www.browserscope.org/user/beacon/' +
-          AWPY.config.browserscope.key +
-          '?sandboxid=' + AWPY.config.browserscope.sandboxKey +
-          '&callback=?';
-    $.getJSON(url, function() {});
-  };
-
-  var get = function() {
-    return list;
-  };
-
   return {
-    init: init,
-    run: run,
-    save: save,
-    get: get
+    init: function(tests) {
+      list = tests;
+    },
+    run: function(callback) {
+      list.forEach(function(test) {
+        test.assert(function(result) {
+          test.result = result;
+          callback.call(test);
+        });
+      });
+    },
+    get: function() {
+      return list;
+    },
+    save: function() {
+      var data = {}, i, len, url;
+      for (i = 0, len = list.length; i < len; i++) {
+        data['AWPY' + i] = list[i].result ? 1 : 0;
+      }
+
+      window._bTestResults = data;
+
+      url = 'http://www.browserscope.org/user/beacon/' +
+            AWPY.config.browserscope.key +
+            '?sandboxid=' + AWPY.config.browserscope.sandboxKey +
+            '&callback=?';
+      $.getJSON(url, function() {});
+    }
   };
 }());
 
-AWPY.sounds = {
-  huge: {
-    duration: 4046210,
-    title: 'Mick Wills (Nation Records) - Demo Mix CD16 - June 2011',
-    artwork_url: 'http://i1.sndcdn.com/artworks-000008609310-c0begl-large.jpg?3588dee',
-    waveform_url: 'http://w1.sndcdn.com/7Rp8J1cZ8RQE_m.png',
-    stream_url: 'http://areweplayingyet.herokuapp.com/sounds/huge.' + AWPY.config.codec,
+AWPY.sound = {
+  duration: 4046210,
+  title: 'Mick Wills (Nation Records) - Demo Mix CD16 - June 2011',
+  artwork_url: 'http://i1.sndcdn.com/artworks-000008609310-c0begl-large.jpg?3588dee',
+  waveform_url: 'http://w1.sndcdn.com/7Rp8J1cZ8RQE_m.png',
+  stream_url: function() {
+   return 'http://areweplayingyet.herokuapp.com/sounds/huge.' + AWPY.config.codec + '?' + (Math.random() * 1e9 | 0);
   }
 };
 
 AWPY.tests.init([
   {
-    name: 'AWPY0',
     description: 'Seeking to unbuffered position with seamless playback.',
+/*
+    1. Start  loading.
+    2. On canplay, seek to the middle of the sound.
+    3. If seek takes more than 500ms => fail
+    4. If playback is not seamless after seek (audio stops for buffering) => fail
+    5. Pass
+*/
     assert: function(finish) {
       var audio = this.audio = new Audio(),
-          sound = AWPY.sounds.huge,
           timeout, finished = false,
           seekedTime;
 
       var finishWrapper = function(result) {
+        if (finished) return;
         finished = true;
         audio.pause();
         audio.src = '';
@@ -89,63 +87,68 @@ AWPY.tests.init([
         finish(result);
       };
 
-      // 1. Load metadata only.
-      // 2. On loadedmetadata, seek to the middle of the sound.
-      // 3. If seek takes more than 500ms => fail
-      // 4. If playback is not seamless after seek (audio stops for buffering) => fail
-      // 5. Pass
+      setTimeout(function() {
+        finishWrapper(false);
+      }, 10000);
 
       audio.volume = 0;
       audio.addEventListener('seeked', function() {
-        if (finished) return;
         clearTimeout(timeout);
-
         seekedTime = audio.currentTime;
         audio.play();
-
         setTimeout(function() {
           finishWrapper(audio.currentTime >= (seekedTime + 2));
         }, 2000);
       }, false);
 
       audio.addEventListener('canplay', function() {
-        audio.currentTime = (sound.duration * 0.5) / 1000;
+        audio.currentTime = (AWPY.sound.duration * 0.5) / 1000;
         timeout = setTimeout(function() {
-          if (finished) return;
           finishWrapper(false);
         }, 500);
       }, false);
 
-      audio.setAttribute('src', sound.stream_url + '?' + (Math.random() * 1e9 | 0));
+      audio.setAttribute('src', AWPY.sound.stream_url());
       audio.load();
     }
   },
   {
-    name: 'AWPY1',
     description: 'Supports preload="metadata"',
+/*
+    1. Set preload to metadata before setting src
+    2. Set src
+    3. If loadedmetadata event is not triggered in less than 3 seconds => fail
+    4. If 2 seconds after loadedmetadata the audio is still buffering (triggering progress) => fail
+    5. Pass
+*/
     assert: function(finish) {
       var audio = this.audio = new Audio(),
-          sound = AWPY.sounds.huge,
           finished = false;
 
+      var finishWrapper = function(result) {
+        if (finished) return;
+        finished = true;
+        finish(result);
+      };
+
+      setTimeout(function() {
+        finishWrapper(false);
+      }, 10000);
 
       audio.addEventListener('loadedmetadata', function() {
         setTimeout(function() {
           audio.addEventListener('progress', function() {
-            if (finished) return;
-            finished = true;
-            finish(false);
+            finishWrapper(false);
           }, false);
 
           setTimeout(function() {
-            if (finished) return;
-            finished = true;
-            finish(true);
+            finishWrapper(true);
           }, 500);
         }, 2000);
       }, false);
+
       audio.setAttribute('preload', 'metadata');
-      audio.setAttribute('src', sound.stream_url + '?' + (Math.random() * 1e9 | 0));
+      audio.setAttribute('src', AWPY.sound.stream_url());
     }
   }
 ]);

334b4a1261a3fd16282be57ad70d4c32ec06bad1
Yves Van Goethem <yves@soundcloud.com>
2011-11-03 18:39:07 +0100

    First tests for events

---
+++ public/tests/event-loadstart.js
@@ -0,0 +1,14 @@
+({
+  name: 'event-loadstart',
+  description: 'Trigger event loadstart',
+  assert: function(finish) {
+    var audio = this.audio = new Audio();
+
+    audio.addEventListener('loadstart', function() {
+      finish( true );
+    }, false);
+
+    audio.setAttribute('preload', 'metadata');
+    audio.setAttribute('src', AWPY.sound.short.stream_url());
+  }
+})

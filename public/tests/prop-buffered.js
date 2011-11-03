df2f0d6791a31f1aa1bfb9c9be2415ea7cb97d6e
Tomás Senart <tomas@soundcloud.com>
2011-11-03 19:56:50 +0100

    Fixed prop buffered

--- public/tests/prop-buffered.js
+++ public/tests/prop-buffered.js
@@ -4,25 +4,13 @@
   spec: 'http://dev.w3.org/html5/spec/Overview.html#dom-media-buffered',
   longdesc: '',
   assert: function(finish) {
-    var audio = this.audio = new Audio(),
-        counter = 0;
-
-    audio.addEventListener('timeupdate', function() {
-      if (++counter >= 5 && audio.currentTime > 0) {
-        try {
-          finish(audio.buffered.length);
-        } catch (e) {
-          finish(false);
-        }
-      }
-    }, false);
+    var audio = this.audio = new Audio();
 
     audio.addEventListener('loadedmetadata', function() {
-      audio.volume = 0;
-      audio.play();
+      finish( audio.buffered && audio.buffered.length );
     }, false);
 
     audio.setAttribute('preload', 'metadata');
-    audio.setAttribute('src', AWPY.sound.long.stream_url(true));
+    audio.setAttribute('src', AWPY.sound.short.stream_url(true));
   }
 })

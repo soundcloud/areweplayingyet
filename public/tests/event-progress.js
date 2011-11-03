a42b1f75c1987283a1bd08f5497d0a5879bcecee
Tomás Senart <tomas@soundcloud.com>
2011-11-03 19:46:07 +0100

    Fix event progress

--- public/tests/event-progress.js
+++ public/tests/event-progress.js
@@ -8,7 +8,7 @@
       finish( true );
     }, false);
 
-    audio.setAttribute('src', AWPY.sound.short.stream_url());
+    audio.setAttribute('src', AWPY.sound.short.stream_url(true));
     audio.load();
   }
 })

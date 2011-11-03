057b8cefa06d9ecc7557a0fd4a5a7d32ee2d6a0f
Tomás Senart <tomas@soundcloud.com>
2011-11-03 20:42:05 +0100

    Add (forgotten) preload-metadata

--- public/tests/prop-seekable.js
+++ public/tests/prop-seekable.js
@@ -4,25 +4,13 @@
   spec: 'http://dev.w3.org/html5/spec/Overview.html#dom-media-seekable',
   longdesc: '',
   assert: function(finish) {
-    var audio = this.audio = new Audio(),
-        counter = 0;
-
-    audio.addEventListener('timeupdate', function() {
-      if (++counter >= 5 && audio.currentTime > 0) {
-        try {
-          finish(audio.seekable.length);
-        } catch (e) {
-          finish(false);
-        }
-      }
-    }, false);
+    var audio = this.audio = new Audio();
 
     audio.addEventListener('loadedmetadata', function() {
-      audio.volume = 0;
-      audio.play();
+      finish( audio.seekable && audio.seekable.length );
     }, false);
 
     audio.setAttribute('preload', 'metadata');
-    audio.setAttribute('src', AWPY.sound.long.stream_url(true));
+    audio.setAttribute('src', AWPY.sound.long.stream_url());
   }
 })

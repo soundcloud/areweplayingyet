5b4457b414d25069e1c1a0857d73bc26a9ea9d97
Tomás Senart <tomas@soundcloud.com>
2011-11-03 14:04:45 +0100

    Moved back to client side rendering.

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-var connect  = require('connect'),
-    mustache = require('mustache'),
-    fs       = require('fs');
+var connect  = require('connect');
 
 var sound = {
   duration: 4046210,
@@ -8,42 +6,12 @@ var sound = {
   download_url: 'https://api.soundcloud.com/tracks/25906673/download?oauth_token=1f267b9842b777a99eb79588d80294b8'
 };
 
-var testTmpl = fs.readFileSync('./tmpl/test.html', 'utf8');
-var homeTmpl = fs.readFileSync('./tmpl/home.html', 'utf8');
-var tests    = {};
-var testsDir = fs.readdir('./public/tests/', function(err, list) {
-  list.forEach(function(file) {
-    tests[file.replace(/\.js/, '')] = fs.readFileSync('./public/tests/' + file, 'utf8');
-  });
-});
-
 connect.createServer(
   connect.logger(),
   connect.router(function(app) {
     app.get('/sound-long.:format/redirect', function(req, res, next) {
       res.statusCode = 303;
       res.setHeader('Location', req.params.format === 'mp3' ? sound.stream_url : sound.download_url);
-      res.end();
-    });
-
-    app.get('/tests/:name', function(req, res, name) {
-      var data = tests[req.params.name];
-      var test = eval(data);
-      res.statusCode = 200;
-      res.write(mustache.to_html(testTmpl, {
-        description: test.description,
-        longdesc: test.longdesc,
-        spec: test.spec,
-        code: test.assert.toString().split('\n').slice(1).slice(0, -1).join('\n'),
-        test: data
-      }));
-      res.end();
-    });
-    app.get('/', function(req, res, name) {
-      res.statusCode = 200;
-      res.write(mustache.to_html(homeTmpl, {
-        tests: Object.keys(tests).map(function(name) { return tests[name]; }).join(',')
-      }));
       res.end();
     });
   }),

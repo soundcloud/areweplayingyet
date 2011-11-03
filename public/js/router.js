cfdc26e5828ee81ab4d588226bd844e05f027811
Tomás Senart <tomas@soundcloud.com>
2011-11-03 14:04:45 +0100

    Moved back to client side rendering.

---
+++ public/js/router.js
@@ -0,0 +1,39 @@
+AWPY.router = {
+  route: function(testName) {
+    var tests = AWPY.tests.get(testName);
+
+    if (tests.length === 1) {
+      this.page = 'single';
+      $('.multi').hide();
+      document.title = tests[0].description + ' | AreWePlayingYet?';
+      $('meta[name="description"]').attr('content', document.title);
+      $('#code').html(tests[0].assert.toString().split('\n').slice(1).slice(0, -1).join('\n'));
+      $('.description').html(tests[0].description);
+      $('.spec').attr('href', tests[0].spec);
+      $('.longdesc').html(tests[0].longdesc);
+      $('.run.big').attr('data-test-name', tests[0].name).html('Run this test!');
+      prettyPrint();
+      $('.single').show();
+    } else {
+      this.page = 'multi';
+      $('.single').hide();
+      $('#tests tbody').html(tests.map(function(test, i) {
+        var testRow = $('#test-row-tmpl').html();
+        Object.keys(test).forEach(function(prop) {
+          testRow = testRow.replace(new RegExp('{{' + prop + '}}', 'g'), test[prop]);
+        });
+        return testRow;
+      }).join(''));
+      $('.run.big').removeAttr('data-test-name').html('Run all tests!');
+      $('.multi').show();
+    }
+  },
+  init: function() {
+    $(window).bind('hashchange', function(ev) {
+      AWPY.runner.teardown();
+      AWPY.router.route(document.location.hash.substring(1));
+      AWPY.runner.init();
+    });
+    $(window).trigger('hashchange');
+  }
+};
--- public/js/runner.js
+++ public/js/runner.js
@@ -1,58 +1,34 @@
-// run on back-end
-AWPY.runner = function() {
-  var list = AWPY.tests.get();
-  console.log(list)
-  var bigBtn = document.getElementById('run');
-  var singleBtns = document.getElementsByClassName('run-single');
-  var arr = function(foo) { return Array.prototype.slice.call(foo); };
+AWPY.runner = {
+  end: function(test, single) {
+    var btnClass = {
+      'TIMEOUT': 'default',
+      'WIN': 'success',
+      'FAIL': 'danger'
+    }[test.result];
+    console.log('end');
+    $('.run[data-test-name="' + test.name + '"]').html(test.result).addClass(btnClass);
 
-  document.getElementById('tests').firstElementChild.innerHTML = list.map(function(test, i) {
-    return '<tr class="test"><td><button class="btn run-single">Run</button></td><td><a href="/tests/' + test.name + '">' + test.description + '</a></td><td><span class="label result">N/A</span></td></tr>';
-  }).join('');
+    if (!single) {
+      var tests = AWPY.tests.get();
+      var score = tests.filter(function(test) {
+        return test.result === 'WIN';
+      }).length;
 
-  var finalize = (function(){
-    var counter = 0, tests = document.getElementsByClassName('test'), score = 0;
-    return function() {
-      var testNode = tests[list.indexOf(this)],
-          resultNode = testNode.getElementsByClassName('result')[0],
-          runBtnSingle = testNode.getElementsByClassName('run-single')[0];
-
-      resultNode.className += (this.result === null ? ' warning' : this.result ? ' success' : ' important');
-      resultNode.innerHTML =  (this.result === null ? 'TIMEOUT' : this.result ? 'WIN' : 'FAIL');
-
-      runBtnSingle.innerHTML = 'Done!';
-
-      if (this.result) {
-        score++;
-      }
-
-      if (++counter === list.length) {
-        arr(singleBtns).concat(bigBtn).forEach(function(btn, i) {
-          btn.innerHTML = 'Done!';
-        });
-
-        bigBtn.innerHTML = 'Score: ' + score + '/' + list.length;
-        // Save results to BrowserScope
-        // AWPY.tests.save();
-        // // Show BrowserScope widget
-        // AWPY.tests.display();
-      }
-    };
-  }());
-
-  [bigBtn].concat(arr(singleBtns)).forEach(function(btn, i, all) {
-    btn.addEventListener('click', function runTest() {
-      btn.removeEventListener('click', runTest, false);
-      btn.className += ' disabled';
-      btn.innerHTML = 'Running…';
-      if (!i) {
-        all.forEach(function(btn) {
-          btn.className += ' disabled';
-          btn.innerHTML = 'Running…';
-        });
-      }
-      AWPY.tests.run(i ? (i - 1) : -1, finalize);
-    }, false)
-  });
-
+      $('.run.big').html('Score: ' + score + '/' + tests.length);
+    }
+  },
+  init: function() {
+    $('.run').one('click', function(ev) {
+      ev.preventDefault();
+      var testName = $(this).data('test-name');
+      var elements = testName ? $(this) : $('.run');
+      elements.addClass('disabled').html('Running');
+      AWPY.tests.run(testName, function(test) {
+        AWPY.runner.end(test, testName);
+      });
+    });
+  },
+  teardown: function() {
+    $('.run').unbind();
+  }
 };

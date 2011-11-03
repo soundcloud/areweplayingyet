8b54ba612b84b3d00ee339b10f9e717a70842b95
Tomás Senart <tomas@soundcloud.com>
2011-11-03 20:03:30 +0100

    Fix prop muted

--- public/tests/prop-muted.js
+++ public/tests/prop-muted.js
@@ -5,14 +5,12 @@
   longdesc: '',
   assert: function(finish) {
     var audio = this.audio = new Audio();
-    audio.addEventListener('loadedmetadata', function() {
-      audio.muted = true;
-      audio.addEventListener('timeupdate', function() {
-        finish( audio.muted === true );
-      }, false);
-    }, false);
 
-    audio.setAttribute('preload', 'metadata');
-    audio.setAttribute('src', AWPY.sound.mini.stream_url());
+    if (!('muted' in audio)) {
+      finish( false );
+    }
+
+    audio.muted = true;
+    finish( audio.muted === true );
   }
 })

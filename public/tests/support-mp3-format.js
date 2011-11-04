f476e1b8be8865bd295ef8421a16c5018d455dff
Yves Van Goethem <yves@soundcloud.com>
2011-11-04 16:55:16 +0100

    Link mp3 test bug reports

--- public/tests/support-mp3-format.js
+++ public/tests/support-mp3-format.js
@@ -1,6 +1,10 @@
 ({
   name: 'support-mp3-format',
   description: 'Supports MP3 format',
+  reports: {
+    firefox: 'https://bugzilla.mozilla.org/show_bug.cgi?id=562730',
+    opera: true
+  },
   assert: function(finish) {
     var audio = this.audio = new Audio();
     finish( audio.canPlayType && (/probably|maybe/).test( audio.canPlayType('audio/mpeg') ) );

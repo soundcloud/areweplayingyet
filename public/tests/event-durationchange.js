a676c672860cddc130c863ce9acfa2f159f6eb84
Tomás Senart <tomas@soundcloud.com>
2011-11-05 19:19:34 +0100

    Revision event-durationchange

--- public/tests/event-durationchange.js
+++ public/tests/event-durationchange.js
@@ -1,19 +1,19 @@
 ({
   name: 'event-durationchange',
   description: 'Event "durationchange"',
+  spec: 'http://www.w3.org/TR/html5/video.html#event-media-durationchange',
   assert: function(finish) {
     var audio = this.audio = new Audio();
 
     audio.addEventListener('durationchange', function() {
-      finish(true);
+      finish(true); // WIN
     }, false);
 
-    audio.addEventListener('loadedmetadata', function() {
-      audio.play();
-    });
+    audio.setAttribute('src', AWPY.sound.mini.stream_url);
+    audio.load(); // Should trigger durationchange soon
 
-    audio.setAttribute('preload', 'metadata');
-    audio.setAttribute('src', AWPY.sound.mini.stream_url(true));
-    audio.volume = 0;
+    setTimeout(function() {
+      finish(false); // FAIL
+    }, 1000);
   }
 })

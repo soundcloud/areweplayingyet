0c6c481acb94a365938033998b0e8215ff403c3a
Yves Van Goethem <yves@soundcloud.com>
2011-11-06 02:34:42 +0100

    Test for emptied event

---
+++ public/tests/event-emptied.js
@@ -0,0 +1,24 @@
+({
+  name: 'event-emptied',
+  description: 'Event "emptied"',
+  spec: 'http://dev.w3.org/html5/spec/the-iframe-element.html#event-media-emptied',
+  assert: function(finish) {
+    var audio = this.audio = new Audio();
+
+    audio.addEventListener('emptied', function() {
+      finish(true);
+    }, false);
+
+    audio.addEventListener('play', function() {
+      audio.setAttribute('src', '');
+      audio.load();
+    }, false);
+
+    audio.addEventListener('loadedmetadata', function() {
+      audio.volume = 0;
+      audio.play();
+    }, false);
+
+    audio.setAttribute('src', AWPY.sound.mini.stream_url());
+  }
+})

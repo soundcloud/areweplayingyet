ef687a09a2596f68ecaf245dd0be9c4f3a74a9be
Tomás Senart <tomas@soundcloud.com>
2011-11-07 13:23:10 +0100

    Added AAC assets, Added AAC test

---
+++ public/tests/support-aac-codec.js
@@ -0,0 +1,8 @@
+({
+  name: 'support-aac-format',
+  description: 'Supports AAC codec',
+  assert: function(finish) {
+    var audio = this.audio = new Audio();
+    finish(/probably|maybe/.test(audio.canPlayType('audio/m4a')));
+  }
+})

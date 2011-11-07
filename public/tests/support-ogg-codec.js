4cfe2c2c88631596e3b570baa068c6b275f97e18
Yves Van Goethem <yves@soundcloud.com>
2011-11-07 13:44:11 +0100

    Fix merge conflict in ogg codec test

--- public/tests/support-ogg-codec.js
+++ public/tests/support-ogg-codec.js
@@ -1,14 +1,12 @@
 ({
-  name: 'support-ogg-format',
-  description: 'Supports OGG format',
+  name: 'support-ogg-codec',
+  description: 'Supports OGG codec',
   reports: {
     safari: {
       desc: 'Bug 42750 - Live OGG streaming using HTML5 audio or video tag does not work ',
       link: 'https://bugs.webkit.org/show_bug.cgi?id=42750#c3'
     }
   },
-  name: 'support-ogg-codec',
-  description: 'Supports OGG codec',
   assert: function(finish) {
     var audio = this.audio = new Audio();
     finish(/probably|maybe/.test(audio.canPlayType('audio/ogg')));

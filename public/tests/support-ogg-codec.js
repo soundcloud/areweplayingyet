f87983c90c2179263afacd36a2cdf6e1c3d4f04c
Tomás Senart <tomas@soundcloud.com>
2011-11-07 13:26:51 +0100

    Renamed support-ogg-format to support-ogg-codec

--- public/tests/support-ogg-codec.js
+++ public/tests/support-ogg-codec.js
@@ -1,6 +1,6 @@
 ({
-  name: 'support-ogg-format',
-  description: 'Supports OGG format',
+  name: 'support-ogg-codec',
+  description: 'Supports OGG codec',
   assert: function(finish) {
     var audio = this.audio = new Audio();
     finish(/probably|maybe/.test(audio.canPlayType('audio/ogg')));

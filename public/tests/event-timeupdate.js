6009b862e1372504c4e7b23c493e2cfa38c21246
Julio C. Menendez <me@juliocarlosmenendez.com>
2012-09-11 10:35:58 -0600

    Fixes URL to the media element event "timeupdate".

--- public/tests/event-timeupdate.js
+++ public/tests/event-timeupdate.js
@@ -1,7 +1,7 @@
 ({
   name: 'event-timeupdate',
   description: 'Event "timeupdate"',
-  spec: 'http://dev.w3.org/html5/spec/the-iframe-element.html#event-media-timeupdate',
+  spec: 'http://dev.w3.org/html5/spec/media-elements.html#event-media-timeupdate',
   assert: function(finish) {
     var audio = this.audio;
 

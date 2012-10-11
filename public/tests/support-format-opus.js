95f451dc7d1391cea018de6e0b93c7923e4b63e2
Yves Van Goethem <yves@soundcloud.com>
2012-10-11 20:34:14 +0200

    Adding Opus spec URL

--- public/tests/support-format-opus.js
+++ public/tests/support-format-opus.js
@@ -1,6 +1,7 @@
 ({
   name: 'support-format-opus',
   description: 'Supports Opus format',
+  spec: 'http://www.opus-codec.org/',
   assert: function(finish) {
     var audio = this.audio;
     finish(/probably|maybe/.test(audio.canPlayType('audio/ogg; codecs="opus"')));
